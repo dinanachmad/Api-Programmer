fa23c71340eb3acac78d651009f562c9e7eb5607
dinan <dinan.achmadf22@gmail.com>
2023-11-16 06:28:59 +0700

    tambahan addcorsmapping

--- ApiProgrammer/src/main/java/com/testNutech/ApiProgrammer/config/SecurityConfig.java
+++ ApiProgrammer/src/main/java/com/testNutech/ApiProgrammer/config/SecurityConfig.java
@@ -13,10 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer{
 	@Autowired
 	private DelegatedAuthenticationEntryPoint authenticationEntryPoint;
 
@@ -55,5 +56,9 @@ public class SecurityConfig {
 				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
 				.build()
 				;
+	}
+	
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 }
